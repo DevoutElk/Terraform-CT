1e30a81cc5463b28e9b75e16e7921fabfd7a0945
crstf-l <usurario.smr@gmail.com>
2026-05-18 20:40:43 +0200

    no nat en apache

--- scripts/install_apache.sh
+++ scripts/install_apache.sh
@@ -7,13 +7,4 @@ apt install -y apache2
 systemctl enable apache2
 systemctl start apache2
 
-echo "==> Configurando red y NAT..."
-echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
-sysctl -p
-DEBIAN_FRONTEND=noninteractive apt install -y iptables-persistent
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables-save > /etc/iptables/rules.v4
-
 echo "==> Frontend listo!"
